1ac1d6c56179a2aaed6bcf5d4195d5932c423d4a
Gabriele Grillo <gabriele.grillo.2003@gmail.com>
2023-06-17 16:25:01 +0200

    Correct commit

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # KòzJudge
 
-Kòz Judge is a basic (also probably stupid) checker for python excerises. Just like Domjudge (aggiungere link)
+![Logo](/multimedia/LOGO.png)
+
+KòzJudge is a basic (also probably stupid) checker for python excerises. Just like Domjudge (aggiungere link)
 
 
-TO DO:
-    - Add time limit
+# TODO LIST: 
+# - Create a sort of debug mode for us
+# - Import timelimit from domjudge-problem.ini
+# - Display name problem
+# - Handle the exception like folder not found or file not found etc..
--- kozjudge.py
+++ kozjudge.py
@@ -1,27 +1,37 @@
 import subprocess, os, glob, configparser
 
 pathEx = ''
-pathSol = ''
+pathSol = '' # Testcases
 timeLimit = 60
 
 def makeConfig(pathExercises: str, pathSolutions: str) -> None:
-    
     config = configparser.ConfigParser()
-    config['PATHS'] = {'exercises': pathExercises, 'solutions': pathSolutions}
+    config['PATHS'] = {'exercises': f"{pathExercises}", 'solutions': f"{pathSolutions}"}
 
     with open('config.ini', 'w') as configfile:
-        configfile.write(config)
+        config.write(configfile)
+
+def loadConfig():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    if (os.path.exists(config['PATHS']['exercises']) and os.path.exists(config['PATHS']['solutions'])):
+        global pathEx, pathSol
+        pathEx = f"{config['PATHS']['exercises']}"
+        pathSol = f"{config['PATHS']['solutions']}"
+    else:
+        print("Error! Check the config file")
+
 
 def loadTestCases(exercise: str) -> list[str]:
     Testcases = []
-    os.chdir(f"/Users/gabrielegrillo/Documents/Università/Primo Semestre/Fondamenti di Programmazione/Esercizi DomJudge/Testcase/{exercise}")
+    os.chdir(f"{pathSol}/{exercise}")
     for file in glob.glob(f"{exercise}_*.in"):
         Testcases.append(file)
     return Testcases
 
 def startJudging(exercise, tc):
     for i in range(len(tc)):
-        file_in = f'/Users/gabrielegrillo/Documents/Università/Primo Semestre/Fondamenti di Programmazione/Esercizi DomJudge/Testcase/{exercise.upper()}/{tc[i]}'
+        file_in = f'{pathSol}/{exercise.upper()}/{tc[i]}'
         file_out = file_in[:-2] + "out"
 
         with open(file_in) as f1:
@@ -32,11 +42,11 @@ def startJudging(exercise, tc):
             innt += inn[j] 
 
         timeoutted = False
-        p = subprocess.Popen(['python3', f'/Users/gabrielegrillo/Documents/Università/Primo Semestre/Fondamenti di Programmazione/Esercizi DomJudge/{exercise}.py'], 
-                            stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+        p = subprocess.Popen(['python3', f'{pathEx}/{exercise}.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+        
         try:
             stdout, stderr = p.communicate(input=innt, timeout=timeLimit)
-        except TimeoutExpired:
+        except subprocess.TimeoutExpired:
             p.kill()
             timeoutted = True
         
@@ -55,20 +65,30 @@ def startJudging(exercise, tc):
             print("Testcase:",tc[i], "WRONG ❌ \n", "Expected output:", str(stdout), "Actual output:", str(outt))
         #print("Testcase:",tc[j], stdout == outt)
 
+def startProgram():
+    print(""" 
+    ██╗░░██╗░█████╗░███████╗░░░░░██╗██╗░░░██╗██████╗░░██████╗░███████╗
+    ██║░██╔╝██╔══██╗╚════██║░░░░░██║██║░░░██║██╔══██╗██╔════╝░██╔════╝
+    █████═╝░██║░░██║░░███╔═╝░░░░░██║██║░░░██║██║░░██║██║░░██╗░█████╗░░
+    ██╔═██╗░██║░░██║██╔══╝░░██╗░░██║██║░░░██║██║░░██║██║░░╚██╗██╔══╝░░
+    ██║░╚██╗╚█████╔╝███████╗╚█████╔╝╚██████╔╝██████╔╝╚██████╔╝███████╗
+    ╚═╝░░╚═╝░╚════╝░╚══════╝░╚════╝░░╚═════╝░╚═════╝░░╚═════╝░╚══════╝\n""")
+    
+    file = input("Welcome to KòzJudge!\nPlease insert the name of the exercise: ")
+    testcase = loadTestCases(file)
+    startJudging(file,testcase)
+
 def main():
-    # Check if it exist a configuration file. Otherwise, create it.
-    if (checkConfig()):
+    if (os.path.exists("config.ini")):
         loadConfig()
-        file = input("Ciao\nInserisci il numero dell'exercise: ")
-        testcase = loadTestCases(file)
-        startJudging(file,testcase)
+        startProgram()
     else:
         pathEx = input("Please insert the path where your exercises are located: ")
         pathSol = input("Please insert the path where your solutions are located: ")
-
+        print()
         makeConfig(pathEx, pathSol)
-
-    
+        loadConfig()
+        startProgram()
 
 
 main()
